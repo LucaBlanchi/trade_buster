875c76f94f0f174932d058f127dd05f92a2a493e
LucaBlanchi <lucablanchi1999@gmail.com>
2025-04-17 02:43:43 +0200

    refactor(main.py): update Wallet initialization to use keyword arguments for clarity and readability refactor(main.py): extract result display logic into a separate function for better organization and maintainability fix(main.py): update starting_worth calculation to use the first record from wallet_records for accuracy

--- src/main.py
+++ src/main.py
@@ -4,16 +4,23 @@ import price_points_constants as ppc
 
 
 def main():
-    starting_wallet = tr.Wallet(1000, 0)
+    starting_wallet = tr.Wallet(cash=1000, assets=0)
     price_points = ppc.SAMPLE_PRICE_POINTS
     starting_index = 0
     strategy = strat.buy_and_hold_strategy
 
-    wallet_records = tr.execute_strategy(strategy, price_points, starting_index, starting_wallet)
+    wallet_records = tr.execute_strategy(
+        strategy, price_points, starting_index, starting_wallet
+    )
+
+    display_results(price_points, starting_index, wallet_records)
+
+
+def display_results(price_points, starting_index, wallet_records):
     for record in wallet_records:
         print(f"Cash: {record.cash}, Assets: {record.assets}")
 
-    starting_worth = starting_wallet.get_worth(price_points[starting_index])
+    starting_worth = wallet_records[0].get_worth(price_points[starting_index])
     final_worth = wallet_records[-1].get_worth(price_points[-1])
 
     print("Final worth:", final_worth)

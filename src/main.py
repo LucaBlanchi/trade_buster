d096ed026d6b63abc8721abe151d78b15fd6ea05
LucaBlanchi <lucablanchi1999@gmail.com>
2025-04-17 02:35:46 +0200

    fix(main.py): update starting index for price points to improve clarity and flexibility in strategy execution

--- src/main.py
+++ src/main.py
@@ -6,13 +6,14 @@ import price_points_constants as ppc
 def main():
     starting_wallet = tr.Wallet(1000, 0)
     price_points = ppc.SAMPLE_PRICE_POINTS
+    starting_index = 0
     strategy = strat.buy_and_hold_strategy
 
-    wallet_records = tr.execute_strategy(strategy, price_points, 0, starting_wallet)
+    wallet_records = tr.execute_strategy(strategy, price_points, starting_index, starting_wallet)
     for record in wallet_records:
         print(f"Cash: {record.cash}, Assets: {record.assets}")
 
-    starting_worth = starting_wallet.get_worth(price_points[0])
+    starting_worth = starting_wallet.get_worth(price_points[starting_index])
     final_worth = wallet_records[-1].get_worth(price_points[-1])
 
     print("Final worth:", final_worth)
